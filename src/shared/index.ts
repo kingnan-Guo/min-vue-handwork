1d33b28416201d89af11013aef020fe2f0ff09db
kingnan <373734199@qq.com>
2023-04-05 20:53:52 +0800

    公共方法 判断 对象是否 改变

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,5 +1,12 @@
+// 将两个或多个对象的属性合并到一起
 export const extend = Object.assign;
 
+// 判断是否 为 object
 export const isObject = (val) => {
     return val !== null && typeof(val) == "object"
+}
+
+// 判断两个对象是否一样
+export const hasChange = (val, newValue) => {
+    return !Object.is(val, newValue)
 }
