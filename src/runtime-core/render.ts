492572f002e3981d9193669677e504b949bc1b56
kingnan <373734199@qq.com>
2023-04-07 16:13:21 +0800

    render 函数中 会调用 path 其中 会给 path 传入 vnode 和 container
    在path 中 先判断 vnode 的数据类型
    判断 vnode 是不是 element 类型 如果是 element 那么会进行数据处理
    在 mountElement 阶段，同样会判断数据类型，如果 vonde中的children 是 string 那么直接将数据 传给textcontent
    如果 vonde中的children 是  array 那么 for循环调用 path 判断 element 还是object....
    
    最后 循环 props 将属性 添加到 dom 节点上
    
    创建dom节点并且将 dom 渲染到 页面上
    
    如果vnode 是 object 那么，会对当前的 vnode 进一步拆解，处理， 执行内部 setUp，处理完成后循环调用 path
    
    -------------
    
    h.ts 此功能调用 createVNode 方法

--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index";
 import { createComponetInstance, setupComponent } from "./components";
 /**
  * 
@@ -18,11 +19,31 @@ export function render(vnode,container) {
  * 2、判断是不是 element 类型
  */
 function patch(vnode, container) {
-
-
-    // 判断是不是 element 类型
-    processComponet(vnode, container)
+    // 判断 vnode 是不是 element 类型
+    // 如果是 element  processElement
+    //
+    // typeof(vnode.type) =string
+    console.log("typeof(vnode.type) ==", typeof(vnode.type));
+    if (typeof vnode.type == 'string') {
+        processElement(vnode, container)
+    } else if(isObject(vnode.type)) {
+        // 如果是 component processComponet
+        // typeof(vnode.type) = object
+        processComponet(vnode, container)
+    }
 }
+
+
+
+/**
+ * 去处理 element
+ * @param vnode 
+ * @param container 
+ */
+function processElement(vnode, container) {
+    mountElement(vnode, container)
+}
+
 /**
  * 去处理组件 processComponet
  * @param vnode 
@@ -32,6 +53,52 @@ function processComponet(vnode: any, container: any) {
     mountComponet(vnode, container)
 }
 
+/**
+ * 
+ * @param vnode 
+ * @param container 
+ */
+function mountElement(vnode, container) {
+    const {type, props, children} = vnode;
+    const el = document.createElement(vnode.type)
+    // 首先区分
+    console.log("typeof(children) ==", typeof(children), 'children ==', children);
+    if (typeof children == "string") {
+        el.textContent = children
+
+    } else if (Array.isArray(children)) {
+        //  每一个children 内部都是 一个虚拟节点vnode ，每一次都要判断是 element 还是 components
+        // children.forEach((vn) => {
+        //     patch(vn, el)
+        // })
+        mountChildren(vnode, el)
+    }
+    console.log("props ==", props);
+    for (const key in props) {
+        const val = props[key]
+        el.setAttribute(key, val)
+    }
+    container.append(el)
+
+}
+
+/**
+ * 处理 children 
+ * @param vnode 虚拟节点vnode 内部存在 children<array> 循环🏪数组 
+ * @param container 容器
+ * 
+ * 每一个children 内部都是 一个虚拟节点vnode ，每一次都要判断是 element 还是 components
+ */
+function mountChildren(vnode, container) {
+    vnode.children.forEach((vn) => {
+        patch(vn, container)
+    })
+}
+/**
+ * 
+ * @param vnode 
+ * @param container 
+ */
 function mountComponet(vnode: any, container: any) {
     // 创建组件实例 
     const instance = createComponetInstance(vnode)
@@ -41,10 +108,12 @@ function mountComponet(vnode: any, container: any) {
 }
 // 
 function setupRenderEffect(instance, container) {
+    console.log("setupRenderEffect  instance =", instance);
+    
     // 调用 render 函数
     // subTree 是虚拟节点树
     const subTree = instance.render()
-    // 基于  render（）  return 出来的 vnode 去调用  patch
+    // 基于  render() return 出来的 vnode 去调用  patch
     // vnode 是 element ，将 element 处理 挂载出来 ，进行 mounElement 处理
 
     // patch 这里是 递归循环调用 ，但现在不知到如何跳出循环
