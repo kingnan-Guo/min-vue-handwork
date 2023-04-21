a9d3e8de124ecedc0159d59eb4344a6cd0861803
kingnan <373734199@qq.com>
2023-04-22 03:40:38 +0800

    修改 createElementVNode 拼写错误，更新快照

--- src/complier-core/src/runtimeHelpers.ts
+++ src/complier-core/src/runtimeHelpers.ts
@@ -1,8 +1,8 @@
 export const TO_DISPLAY_STRING = Symbol("toDisplayString")
-export const CREATE_ELEMENT_VNODE = Symbol("creatElementVNode")
+export const CREATE_ELEMENT_VNODE = Symbol("createElementVNode")
 
 // TO_DISPLAY_STRING 转换成string
 export const helperMapName = {
     [TO_DISPLAY_STRING]:"toDisplayString",
-    [CREATE_ELEMENT_VNODE]:"creatElementVNode",
+    [CREATE_ELEMENT_VNODE]:"createElementVNode",
 }
