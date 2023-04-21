2c9ef5ee8ede5893d7ea13216b8ffcc64dc7554e
kingnan <373734199@qq.com>
2023-04-22 01:40:56 +0800

    检测 转换 element 为 ast 树  的 方法是否正确

--- src/complier-core/__test__/parse.spec.ts
+++ src/complier-core/__test__/parse.spec.ts
@@ -51,6 +51,8 @@ describe("Parse", () => {
       });
 
 
+      
+
 });
   
 
@@ -112,6 +114,18 @@ test("linke - p", () => {
 
 
 })
+
+describe("element", () => {
+  it("simple element div", () => {
+    const ast = baseParse("<div></div>");
+
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: "div",
+      children: [],
+    });
+  });
+});
 
 
 // test("should throw error when lack end tag", () => {
